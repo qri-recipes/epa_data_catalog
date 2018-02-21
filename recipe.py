9e9e1bc199fba62284b9076cfae65a6866bcd7f6
Thomas Osterbind <tom@qri.io>
2018-02-21 17:54:02 -0500

    refactor: rewritten to avoid use of global variables for primary params

--- recipe.py
+++ recipe.py
@@ -2,30 +2,16 @@ import os
 from subprocess import Popen, PIPE
 import shlex
 import datetime
+import time
 import sys
+import json
+import requests
+import re
 
-# set constants
 _MAX_ATTEMPTS = 10
 _DELAY = .1
 NOW = datetime.datetime.now()
-
-QRI_COMMAND_TEMPLATE="""qri {action} \
---data "{DATA_PATH}" \
---structure "{STRUCTURE_PATH}" \
---meta "{META_PATH}" \
-me/{DATASET_NAME}
-"""
-
-# get variables from env
-try:  
-   DATASET_NAME   = os.environ["r_dataset_name"]
-   TARGET_URL     = os.environ["r_target_url"]
-   DATA_PATH      = os.environ["r_data_path"]
-   STRUCTURE_PATH = os.environ["r_structure_path"]
-   META_PATH      = os.environ["r_meta_path"]
-   TEST_RUN       = True if os.environ["r_test"] == "True" else False
-except KeyError as e: 
-   print "Please ensure all required environment variales are set: missing {}".format(e)
+TIME_STAMP = datetime.datetime.strftime(NOW, "%Y-%m-%dT%H:%M:%S")
 
 # --------------------------------------------------------------------
 def _shell_exec_once(command):
@@ -46,53 +32,103 @@ def _shell_exec(command):
 
 # --------------------------------------------------------------------
 
-# get data
-def fetch_data():
-	#rename file if exists already
-	if os.path.exists(DATA_PATH):
-		cmd = "mv {path} prev_{path}".format(path=DATA_PATH)
-		_shell_exec(cmd)
-	#download file
-	cmd = "wget -O {path} {url}".format(path=DATA_PATH, url=TARGET_URL)
-	print("fetching data from '{}'...".format(TARGET_URL))
-	result=_shell_exec(cmd)
-	#print(result)
-	print("download complete")
+# fetch_data retrieves a data.json file from the target url and saves
+#   it to the data_path provided
+def fetch_data(data_path, url):
+  #rename file if exists already
+  if os.path.exists(data_path):
+    cmd = "mv {path} prev_{path}".format(path=data_path)
+    _shell_exec(cmd)
+  #download file
+  print("fetching data from '{}'...".format(url))
+  response = requests.get(url)
+  data_string = response.text
+  with open(data_path, "w") as fp:
+    fp.write(data_string)
+  print("download complete")
+
+# update_meta_timestamp modifies the 'updated' field of the file at 
+#   the meta_path with the string provided as timestamp
+def update_meta_timestamp(meta_path, timestamp):
+  with open(meta_path, "r") as fp:
+    meta_dict = json.load(fp)
+    meta_dict[u"updated"] = u"{}".format(timestamp)
+  with open(meta_path, "w") as fp:
+    fp.write(json.dumps(meta_dict, indent=2))
+
+# _dataset_exists checks whether a dataset exists by attempting to 
+#   request info on the dataset and seeing if it errors or not
+def _dataset_exists(dataset_name):
+  exists = True
+  cmd = "qri info me/{}".format(dataset_name)
+  result = _shell_exec_once(cmd)
+  for line in result.split("\n"):
+    if re.match(r'^error', line):
+      exists = False
+      break
+  return exists
+
+# --------------------------------------------------------------------
+
+def add_qri_dataset(dataset_name, data_path, structure_path, meta_path):
+  cmd = "qri add "
+  cmd += "--data \"{}\" ".format(data_path)
+  cmd += "--structure \"{}\" ".format(structure_path)
+  cmd += "--meta \"{}\" ".format(meta_path)
+  cmd += "me/{} ".format(dataset_name)
+  # result = _shell_exec(cmd)
+  result = _shell_exec_once(cmd)
+  return result
 
 
-def _dataset_exists(DATASET_NAME):
-	cmd = "qri info me/{} | grep ^error".format(DATASET_NAME)
-	result = _shell_exec(cmd)
-	if result == "":
-		return True
-	else:
-		return False
+def update_qri_dataset(dataset_name, data_path, structure_path, meta_path, commit_msg):
+  cmd = "qri save "
+  cmd += "-m \"{}\" ".format(commit_msg)
+  cmd += "--data \"{}\" ".format(data_path)
+  cmd += "--structure \"{}\" ".format(structure_path)
+  cmd += "--meta \"{}\" ".format(meta_path)
+  cmd += "me/{} ".format(dataset_name)
+  # result = _shell_exec(cmd)
+  result = _shell_exec_once(cmd)
+  return result
+
+def add_or_save_to_qri(dataset_name, data_path, structure_path, meta_path):
+  if _dataset_exists(dataset_name):
+    # set commit message and choose 'save'
+    commit_msg = "recipe update @ {}".format(TIME_STAMP)
+    result = update_qri_dataset(dataset_name, data_path, structure_path, meta_path, commit_msg)
+  else:
+    result = add_qri_dataset(dataset_name, data_path, structure_path, meta_path)
+  return result
 
 
-def add_or_save_to_qri():
-	action = "add"
-	if _dataset_exists(DATASET_NAME):
-		#add commit message and choose 'save'
-		date_string = datetime.datetime.strftime(NOW, "%Y-%m-%dT%H:%M:%S")
-		message = "recipe update @ {}".format(date_string)
-		action = "save -m=\"{}\"".format(message)
-	params = dict(
-		action=action,
-		DATA_PATH=DATA_PATH,
-		STRUCTURE_PATH=STRUCTURE_PATH,
-		META_PATH=META_PATH,
-		DATASET_NAME=DATASET_NAME,
-		)
-	cmd = QRI_COMMAND_TEMPLATE.format(**params)
-	result = _shell_exec(cmd)
-	print(result)
-	#print cmd
-
+# --------------------------------------------------------------------
 def main():
-	if not TEST_RUN:
-		fetch_data()
-	add_or_save_to_qri()
+  # get variables from env
+  try:  
+     DATASET_NAME   = os.environ["r_dataset_name"]
+     TARGET_URL     = os.environ["r_target_url"]
+     DATA_PATH      = os.environ["r_data_path"]
+     STRUCTURE_PATH = os.environ["r_structure_path"]
+     META_PATH      = os.environ["r_meta_path"]
+     TEST_RUN       = True if os.environ["r_test"] == "True" else False
+  except KeyError as e: 
+     print "Please ensure all required environment variales are set: missing {}".format(e)
+  # prepare data.json
+  if not TEST_RUN:
+    fetch_data(DATA_PATH, TARGET_URL)
+  if not os.path.exists(DATA_PATH):
+    print("Unable to find '{}'".format(DATA_PATH))
+  # prepare meta.json
+  update_meta_timestamp(META_PATH, TIME_STAMP)
+  # add or save dataset on qri
+  output = add_or_save_to_qri(DATASET_NAME, DATA_PATH, STRUCTURE_PATH, META_PATH)
+  # revert metadata.json to prevent udpates from being tracked in version control
+  update_meta_timestamp(META_PATH,"")
+  print(output)
 
+
+  
 
 if __name__ == "__main__":
-	main()
+  main()
--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 # TODO: remove the following line after docker is set up
 # TODO: remove 'export' from recipe_config.env
 source recipe_config.env
+
+# install python dependencies
+pip install -r requirements.txt
+
+#execute
 python recipe.py
