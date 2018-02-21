2b6da8f1663a279bd04200532dd001e8bd603760
Thomas Osterbind <tom@qri.io>
2018-02-21 13:42:06 -0500

    chore: changed constant names to all caps

--- recipe.py
+++ recipe.py
@@ -10,23 +10,22 @@ _DELAY = .1
 NOW = datetime.datetime.now()
 
 QRI_COMMAND_TEMPLATE="""qri {action} \
---data "{data_path}" \
---structure "{structure_path}" \
---meta "{meta_path}" \
-me/{dataset_name}
+--data "{DATA_PATH}" \
+--structure "{STRUCTURE_PATH}" \
+--meta "{META_PATH}" \
+me/{DATASET_NAME}
 """
 
 # get variables from env
 try:  
-   dataset_name   = os.environ["r_dataset_name"]
-   target_url     = os.environ["r_target_url"]
-   data_path      = os.environ["r_data_path"]
-   structure_path = os.environ["r_structure_path"]
-   meta_path      = os.environ["r_meta_path"]
-   test_run       = True if os.environ["r_test"] == "True" else False
-except KeyError: 
-   print "Please ensure all required environment variales are set"
-   sys.exit(1)
+   DATASET_NAME   = os.environ["r_dataset_name"]
+   TARGET_URL     = os.environ["r_target_url"]
+   DATA_PATH      = os.environ["r_data_path"]
+   STRUCTURE_PATH = os.environ["r_structure_path"]
+   META_PATH      = os.environ["r_meta_path"]
+   TEST_RUN       = True if os.environ["r_test"] == "True" else False
+except KeyError as e: 
+   print "Please ensure all required environment variales are set: missing {}".format(e)
 
 # --------------------------------------------------------------------
 def _shell_exec_once(command):
@@ -50,19 +49,19 @@ def _shell_exec(command):
 # get data
 def fetch_data():
 	#rename file if exists already
-	if os.path.exists(data_path):
-		cmd = "mv {path} prev_{path}".format(path=data_path)
+	if os.path.exists(DATA_PATH):
+		cmd = "mv {path} prev_{path}".format(path=DATA_PATH)
 		_shell_exec(cmd)
 	#download file
-	cmd = "wget -O {path} {url}".format(path=data_path, url=target_url)
-	print("fetching data from '{}'...".format(target_url))
+	cmd = "wget -O {path} {url}".format(path=DATA_PATH, url=TARGET_URL)
+	print("fetching data from '{}'...".format(TARGET_URL))
 	result=_shell_exec(cmd)
 	#print(result)
 	print("download complete")
 
 
-def _dataset_exists(dataset_name):
-	cmd = "qri info me/{} | grep ^error".format(dataset_name)
+def _dataset_exists(DATASET_NAME):
+	cmd = "qri info me/{} | grep ^error".format(DATASET_NAME)
 	result = _shell_exec(cmd)
 	if result == "":
 		return True
@@ -72,17 +71,17 @@ def _dataset_exists(dataset_name):
 
 def add_or_save_to_qri():
 	action = "add"
-	if _dataset_exists(dataset_name):
+	if _dataset_exists(DATASET_NAME):
 		#add commit message and choose 'save'
 		date_string = datetime.datetime.strftime(NOW, "%Y-%m-%dT%H:%M:%S")
 		message = "recipe update @ {}".format(date_string)
 		action = "save -m=\"{}\"".format(message)
 	params = dict(
 		action=action,
-		data_path=data_path,
-		structure_path=structure_path,
-		meta_path=meta_path,
-		dataset_name=dataset_name,
+		DATA_PATH=DATA_PATH,
+		STRUCTURE_PATH=STRUCTURE_PATH,
+		META_PATH=META_PATH,
+		DATASET_NAME=DATASET_NAME,
 		)
 	cmd = QRI_COMMAND_TEMPLATE.format(**params)
 	result = _shell_exec(cmd)
@@ -90,7 +89,7 @@ def add_or_save_to_qri():
 	#print cmd
 
 def main():
-	if not test_run:
+	if not TEST_RUN:
 		fetch_data()
 	add_or_save_to_qri()
 
