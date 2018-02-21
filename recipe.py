309b54528385ad67ed23bc0d1eb5c676f67c96a4
Thomas Osterbind <tom@qri.io>
2018-02-21 10:46:52 -0500

    refactor: converted shell script to python script

--- recipe.py
+++ recipe.py
@@ -1,5 +1,6 @@
 import os
-import subprocess
+from subprocess import Popen, PIPE
+import shlex
 import datetime
 import sys
 
@@ -10,8 +11,8 @@ NOW = datetime.datetime.now()
 
 QRI_COMMAND_TEMPLATE="""qri {action} \
 --data "{data_path}" \
---structure "{structure_path} \
---meta "{meta_path} \
+--structure "{structure_path}" \
+--meta "{meta_path}" \
 me/{dataset_name}
 """
 
@@ -22,6 +23,7 @@ try:
    data_path      = os.environ["r_data_path"]
    structure_path = os.environ["r_structure_path"]
    meta_path      = os.environ["r_meta_path"]
+   test_run       = True if os.environ["r_test"] == "True" else False
 except KeyError: 
    print "Please ensure all required environment variales are set"
    sys.exit(1)
@@ -45,6 +47,19 @@ def _shell_exec(command):
 
 # --------------------------------------------------------------------
 
+# get data
+def fetch_data():
+	#rename file if exists already
+	if os.path.exists(data_path):
+		cmd = "mv {path} prev_{path}".format(path=data_path)
+		_shell_exec(cmd)
+	#download file
+	cmd = "wget -O {path} {url}".format(path=data_path, url=target_url)
+	print("fetching data from '{}'...".format(target_url))
+	result=_shell_exec(cmd)
+	#print(result)
+	print("download complete")
+
 
 def _dataset_exists(dataset_name):
 	cmd = "qri info me/{} | grep ^error".format(dataset_name)
@@ -54,12 +69,34 @@ def _dataset_exists(dataset_name):
 	else:
 		return False
 
-action = "add"
-if _dataset_exists(dataset_name):
-	#add commit message and choose 'save'
-	date_string = datetime.datetime.strftime(NOW, "%Y-%m-%dT%H:%M:%S")
-	message = "recipe update @ {}".format(date_string)
-	action = "save -m=\"{}\"".format(message)
+
+def add_or_save_to_qri():
+	action = "add"
+	if _dataset_exists(dataset_name):
+		#add commit message and choose 'save'
+		date_string = datetime.datetime.strftime(NOW, "%Y-%m-%dT%H:%M:%S")
+		message = "recipe update @ {}".format(date_string)
+		action = "save -m=\"{}\"".format(message)
+	params = dict(
+		action=action,
+		data_path=data_path,
+		structure_path=structure_path,
+		meta_path=meta_path,
+		dataset_name=dataset_name,
+		)
+	cmd = QRI_COMMAND_TEMPLATE.format(**params)
+	result = _shell_exec(cmd)
+	print(result)
+	#print cmd
+
+def main():
+	if not test_run:
+		fetch_data()
+	add_or_save_to_qri()
+
+
+if __name__ == "__main__":
+	main()
 
 # get data
 # if a data file exists rename it to avoid wget naming conflict
---
+++ run.sh
@@ -0,0 +1,4 @@
+# TODO: remove the following line after docker is set up
+# TODO: remove 'export' from recipe_config.env
+source recipe_config.env
+python recipe.py
